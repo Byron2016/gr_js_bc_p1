2c9e60fa7ecae7cf0b9d984eb97155e65740ed42
Byron2016 <bgva2005@yahoo.com>
2023-04-05 13:57:04 -0500

    chore: first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+<div>
+	<div>
+		<img src=https://raw.githubusercontent.com/Byron2016/00_forImages/main/images/Logo_01_00.png align=left alt=MyLogo width=200>
+	</div>
+	&nbsp;
+	<div>
+		<h1>024_gr_js_bc_p01</h1>
+	</div>
+</div>
+
+&nbsp;
+
+# Table of contents
+
+- [Table of contents](#table-of-contents)
+- [Project Description](#project-description)
+- [Technologies used](#technologies-used)
+- [References](#references)
+- [Steps](#steps)
+
+  - <details> <summary>List of Steps</summary>
+
+    - [Install & Setup Vite + React + Bootstrap 5](#-artificial-intelligence-and-bots)
+
+   </details>
+
+[⏪(Back to top)](#table-of-contents)
+
+# Project Description
+
+**024_gr_js_bc_p01** is a practice to build a **Responsive Portfolio Website** with HTLM, CSS, bootstrap 5 following Youtube Guillermo Rodas's tutorial [⛺️ Bootcamp — Practica 1, Clase 1: HTML Semántico y SASS](https://www.youtube.com/watch?v=T0KfCFzY800&list=PLKBvDYynKmlW1GrqNnwjdgoGP6PlYkxrh&index=20) and the other help that you can find into **Reference** section.
+
+---
+
+[⏪(Back to top)](#table-of-contents)
+&nbsp;
+
+# Technologies used
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)
+
+[⏪(Back to top)](#table-of-contents)
+
+# References
+
+- Guillermo Rodas's tutorial
+
+  - [⛺️ Bootcamp — Practica 1, Clase 1: HTML Semántico y SASS](https://www.youtube.com/watch?v=T0KfCFzY800&list=PLKBvDYynKmlW1GrqNnwjdgoGP6PlYkxrh&index=20)
+  - [undefined-academy](https://github.com/glrodasz/undefined-academy/tree/main/practica-blog)
+
+- Shields.io
+
+  - [Shields.io](https://shields.io/)
+
+  - [Github Ileriayo markdown-badges](https://github.com/Ileriayo/markdown-badges)
+
+  - [Github Ileriayo markdown-badges WebSite](https://ileriayo.github.io/markdown-badges/)
+
+[⏪(Back to top)](#table-of-contents)
+
+# Steps
+
+---
+
+[⏪(Back to top)](#table-of-contents)
---
+++ script.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+# To run: bash script.sh  
+echo "<div>" >> README.md
+echo "	<div>" >> README.md
+echo "		<img src="https://raw.githubusercontent.com/Byron2016/00_forImages/main/images/Logo_01_00.png" align="left" alt="MyLogo" width="200">" >> README.md
+echo "	</div>" >> README.md
+echo "	&nbsp;" >> README.md
+echo "	<div>" >> README.md
+echo "		<h1>024_gr_js_bc_p01</h1>" >> README.md
+echo "	</div>" >> README.md
+echo "</div>" >> README.md
+echo "" >> README.md
+echo "&nbsp;" >> README.md
+
+echo "" >> README.md
+echo "# Table of contents" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "- [Table of contents](#table-of-contents)" >> README.md
+echo "- [Project Description](#project-description)" >> README.md
+echo "- [Technologies used](#technologies-used)" >> README.md
+echo "- [References](#references)" >> README.md
+echo "- [Steps](#steps)" >> README.md
+echo "" >> README.md
+echo "  - <details> <summary>List of Steps</summary>" >> README.md
+echo "" >> README.md
+echo "    - [Install & Setup Vite + React + Bootstrap 5](#-artificial-intelligence-and-bots)" >> README.md
+echo "" >> README.md
+echo "   </details>" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# Project Description" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+echo "&nbsp;" >> README.md
+
+
+echo "" >> README.md
+echo "# Technologies used" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)" >> README.md
+echo "![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)" >> README.md
+echo "![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)" >> README.md
+echo "![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)" >> README.md
+echo "![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)" >> README.md
+echo "![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# References" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "- Shields.io" >> README.md
+echo "" >> README.md
+echo "  - [Shields.io](https://shields.io/)" >> README.md
+echo "" >> README.md
+echo "  - [Github Ileriayo markdown-badges](https://github.com/Ileriayo/markdown-badges)" >> README.md
+echo "" >> README.md
+echo "  - [Github Ileriayo markdown-badges WebSite](https://ileriayo.github.io/markdown-badges/)" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# Steps" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "  " >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+# upload to github 
+
+git init
+git add .
+git commit -m "chore: first commit"
+git branch -M main
+git remote add origin https://github.com/Byron2016/gr_js_bc_p1.git
+git push -u origin main
+
